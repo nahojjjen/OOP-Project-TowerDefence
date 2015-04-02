5ac40861379dfe4e7b0e4acf6e85f8ed3084f3c3
nahojjjen <johansw95@gmail.com>
2015-04-02 10:18:58 +0200

    uploads new library - libgdx

---
+++ ProjectFiles/desktop/src/edu/chl/proximity/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.chl.proximity.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.chl.proximity.Proximity;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Proximity(), config);
+	}
+}
