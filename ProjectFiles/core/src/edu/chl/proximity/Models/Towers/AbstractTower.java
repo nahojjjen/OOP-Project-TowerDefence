81ca8ec154748b22b62d4303a639debde430c209
nahojjjen <johansw95@gmail.com>
2015-04-02 11:17:39 +0200

    firstTowerFiles

---
+++ ProjectFiles/core/src/edu/chl/proximity/Models/Towers/AbstractTower.java
@@ -0,0 +1,9 @@
+package edu.chl.proximity.Models.Towers;
+
+import edu.chl.proximity.Models.BoardObject;
+
+/**
+ * Created by Johan on 4/2/2015.
+ */
+public class AbstractTower extends BoardObject {
+}
---
+++ ProjectFiles/core/src/edu/chl/proximity/Utilities/Image.java
@@ -0,0 +1,12 @@
+package edu.chl.proximity.Utilities;
+
+import com.badlogic.gdx.graphics.Texture;
+
+/**
+ * Created by Johan on 4/2/2015.
+ */
+public class Image {
+    private Texture file;
+
+
+}
