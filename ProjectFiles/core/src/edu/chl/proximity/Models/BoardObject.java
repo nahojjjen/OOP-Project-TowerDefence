d36e5f40cb081d3ee8113e91dfb41b8392caf097
nahojjjen <johansw95@gmail.com>
2015-04-02 10:56:12 +0200

    Uploads folder structure for project

---
+++ ProjectFiles/core/src/edu/chl/proximity/Models/BoardObject.java
@@ -0,0 +1,7 @@
+package edu.chl.proximity.Models;
+
+/**
+ * Created by Johan on 4/2/2015.
+ */
+public class BoardObject {
+}
---
+++ ProjectFiles/core/src/edu/chl/proximity/Utilities/Constants.java
@@ -0,0 +1,7 @@
+package edu.chl.proximity.Utilities;
+
+/**
+ * Created by Johan on 4/2/2015.
+ */
+public class Constants {
+}
