5f0106c8263eb78463e66dd89949a9558029f81d
nahojjjen <johansw95@gmail.com>
2015-04-11 13:21:44 +0200

    Removes unusable legacy methods
    Group work with linda

--- ProjectFiles/core/src/edu/chl/proximity/Viewers/Renderer.java
+++ ProjectFiles/core/src/edu/chl/proximity/Viewers/Renderer.java
@@ -5,7 +5,9 @@ import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.ParticleEffectPool;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import edu.chl.proximity.Models.Creeps.Creep;
+import edu.chl.proximity.Models.GameData;
 import edu.chl.proximity.Models.Maps.Map;
+import edu.chl.proximity.Models.Particles.ParticleManager;
 import edu.chl.proximity.Models.Projectiles.Projectile;
 import edu.chl.proximity.Models.Towers.Tower;
 
@@ -25,8 +27,8 @@ public class Renderer {
 
     private Map map;
 
-    public Renderer(Map map) {
-        this.map = map;
+    public Renderer() {
+        this.map = GameData.getInstance().getMap();
     }
 
     public void render(SpriteBatch batch) {
@@ -39,50 +41,10 @@ public class Renderer {
         renderProjectiles(batch);
         renderParticles(batch);
 
-
-        //debuggRenderWaypoints();
-        //debuggRenderCreepAngle();
-
     }
-
-
-    /*
-    public static void debuggRenderPathHitbox(Graphics g){
-        Path path = new Path();
-        List<Rectangle> hitboxes = path.getHitbox();
-        ShapeRenderer renderer =  new ShapeRenderer();
-        for(Rectangle rectangle : hitboxes){
-            renderer.begin();
-            renderer.setColor(1,1,1,1);
-            renderer.rect(rectangle.getX(),rectangle.getY(),rectangle.getWidth(),rectangle.getHeight());
-           }
-    }
-    */
-
-    /*
-    private static void debuggRenderCreepAngle(){
-        List<Creep> creeps = Map.getInstance().getCreeps();
-        Image line = Debug.getLine();
-        for (Creep creep : creeps) {
-            line.setRotation((float) creep.getAngle());
-            line.draw(creep.getx()-20,creep.gety()-20);
-        }
-    }
-    */
-    /*
-    private static void debuggRenderWaypoints(){
-        Path path = Map.getPath();
-        List<Point> wayPoints = path.getWaypoints();
-        Image image = Debug.getWaypoint();
-        for (Point point: wayPoints){
-            image.draw(point.getX(),point.getY());
-        }
-    }
-    */
 
     private void renderPath(Batch batch){
-//         batch.draw(Map.getInstance().getPath().getImage(), 0, Gdx.graphics.getHeight());
-
+        //   batch.draw(Map.getInstance().getPath().getImage(), 0, Gdx.graphics.getHeight());
     }
     private void renderBackground(Batch batch) {
         //batch.draw(Map.getInstance().);
@@ -95,13 +57,13 @@ public class Renderer {
 
     private void renderTowers(SpriteBatch batch)  {
         List<Tower> towers = map.getTowers();
-   if (towers != null){
-       for (Tower tower : towers) {
-           //tower.getAnimation().draw(tower.getPoint().getX()-20, tower.getPoint().getY()-20);
-           tower.getImage().render(batch, tower.getPosition(), tower.getAngle());
+           if (towers != null){
+               for (Tower tower : towers) {
+                   //tower.getAnimation().draw(tower.getPoint().getX()-20, tower.getPoint().getY()-20);
+                   tower.getImage().render(batch, tower.getPosition(), tower.getAngle());
 
-       }
-   }
+               }
+        }
 
     }
 
@@ -130,17 +92,7 @@ public class Renderer {
 
 
     private  void renderParticles(SpriteBatch batch)   {
-        List<ParticleEffectPool.PooledEffect> explosions = map.getExplosions();
 
-        for (int i = explosions.size() - 1; i >= 0; i--) {
-            ParticleEffectPool.PooledEffect explosion = explosions.get(i);
-            explosion.draw(batch, Gdx.graphics.getDeltaTime());
-            if(explosion.isComplete()){
-                System.out.println("In renderer: Explosion effect is marked complete and removed");
-                explosion.free();
-                explosions.remove(explosion);
-            }
-        }
     }
 
 }
