57a6601bdd993be396beebd2a9b4467958317e6a
IndigoBB <hanna.romer@gmail.com>
2015-04-15 13:49:45 +0200

    Added java-doc

--- ProjectFiles/core/src/edu/chl/proximity/Models/ResourceSystem/Resources.java
+++ ProjectFiles/core/src/edu/chl/proximity/Models/ResourceSystem/Resources.java
@@ -1,45 +1,102 @@
 package edu.chl.proximity.Models.ResourceSystem;
 
 /**
- * Created by Hanna on 2015-04-15.
+ * @Author Hanna Römer
+ * @Date 2015-04-15.
+ * Class containing the different resources in the game; points, lines and polygons
  */
 public class Resources {
     private int points;
     private int lines;
     private int polygons;
 
+    /**
+     * Create a new instance of Resources
+     * @param startPoints Starting amount of points
+     * @param startLines Starting amount of lines
+     * @param startPolygons Starting amount of polygons
+     */
     public Resources(int startPoints, int startLines, int startPolygons ){
         points=startPoints;
         lines=startLines;
         polygons=startPolygons;
     }
 
+    /**
+     * Get current amount of points
+     * @return current amount of points
+     */
     public int getPoints() {
         return points;
     }
+
+    /**
+     * Get current amount of lines
+     * @return current amount of lines
+     */
     public int getLines() {
         return lines;
     }
+
+    /**
+     * Get current amount of polygons
+     * @return current amount of polygons
+     */
     public int getPolygons() {
         return polygons;
     }
 
+    /**
+     * Add given amount of points to current
+     * amount of points
+     * @param p amount of points to add
+     */
     public void addPoints(int p){
         points += p;
     }
+
+    /**
+     * Add given amount of lines to
+     * current amount of lines
+     * @param l amount of lines to add
+     */
     public void addLines(int l){
         lines += l;
     }
+
+    /**
+     * Add given amount of polygons to
+     * current amount of polygons
+     * @param p amount of polygons to add
+     */
     public void addPolygons(int p){
         polygons += p;
     }
 
+
+    /**
+     * Remove given amount of points to current
+     * amount of points
+     * @param p amount of points to remove
+     */
     public void removePoints(int p){
         points -= p;
     }
+
+    /**
+     * Remove given amount of lines to
+     * current amount of lines
+     * @param l amount of lines to remove
+     */
     public void removeLines(int l){
         lines -= l;
     }
+
+    /**
+     * Remove given amount of polygons to
+     * current amount of polygons
+     * @param p amount of polygons to remove
+     */
     public void removePolygons(int p){
         polygons -=p;
     }
