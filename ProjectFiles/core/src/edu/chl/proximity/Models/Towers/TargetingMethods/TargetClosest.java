1817b62921b54a1a576098eb8fca1b9aabf31274
IndigoBB <hanna.romer@gmail.com>
2015-04-17 10:13:58 +0200

    Range now matters again

--- ProjectFiles/core/src/edu/chl/proximity/Models/Towers/TargetingMethods/TargetClosest.java
+++ ProjectFiles/core/src/edu/chl/proximity/Models/Towers/TargetingMethods/TargetClosest.java
@@ -22,17 +22,26 @@ public class TargetClosest extends TargetingMethod{
     public Creep getTarget(Vector2 position, double range){
         double rangeToClosest;
         Creep closestCreep;
+
         if(map.getCreeps().size() > 0){
-            closestCreep = map.getCreeps().get(0);
+            List<Creep> inRange = new ArrayList<Creep>();
+            for(Creep c: map.getCreeps()){
+                if(isWithinRange(c,position,range)){
+                    inRange.add(c);
+                }
+            }
+            if(inRange.size() <= 0){
+                return null;
+            }
+
+            closestCreep = inRange.get(0);
             rangeToClosest = PointCalculations.distanceBetweenNoSqrt(closestCreep.getPosition(), position);
 
-            for (Creep creep:map.getCreeps()){
+            for (Creep creep:inRange){
                 double rangeToCurrent = PointCalculations.distanceBetweenNoSqrt(position, creep.getPosition());
-                if (isWithinRange(creep, position, range)){
-                    if (rangeToCurrent < rangeToClosest){
-                        rangeToClosest = rangeToCurrent;
-                        closestCreep = creep;
-                    }
+                if (rangeToCurrent < rangeToClosest){
+                    rangeToClosest = rangeToCurrent;
+                    closestCreep = creep;
                 }
             }
 
--- ProjectFiles/core/src/edu/chl/proximity/Models/Towers/TargetingMethods/TargetFirst.java
+++ ProjectFiles/core/src/edu/chl/proximity/Models/Towers/TargetingMethods/TargetFirst.java
@@ -6,6 +6,7 @@ import edu.chl.proximity.Models.GameData;
 import edu.chl.proximity.Models.Maps.Map;
 import edu.chl.proximity.Utilities.PointCalculations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -26,13 +27,22 @@ public class TargetFirst extends TargetingMethod{
     @Override
     public Creep getTarget(Vector2 towerPosition, double towerRange) {
         if (creeps.size() > 0){ //can only get a target if there are enemies
-            Creep target = creeps.get(0); //start with a creep so a comparison can be made
+            List<Creep> inRange = new ArrayList<Creep>();
+            for(Creep c: creeps){
+                if(isWithinRange(c,towerPosition,towerRange)){
+                    inRange.add(c);
+                }
+            }
+            if(inRange.size() <= 0){
+                return null;
+            }
+            Creep target = inRange.get(0); //start with a creep so a comparison can be made
             double distanceToWaypoint = 9999999999.9; //the first creep does not have to be checked, more efficient to hard-code in dummy data
             int waypointNumber = 0;
 
             //cycle through all creeps, check if they're within range, get what waypoint they're on, remember the one with the highest waypoint & shortest distance to waypoint
-            for (Creep creep:creeps){
-                    if (isWithinRange(creep,towerPosition,towerRange) && creep.getDistanceToNextWayPoint() < distanceToWaypoint && creep.getNextWayPointID() > waypointNumber){
+            for (Creep creep:inRange){
+                    if (creep.getDistanceToNextWayPoint() < distanceToWaypoint && creep.getNextWayPointID() > waypointNumber){
                         target = creep;
                         distanceToWaypoint = creep.getDistanceToNextWayPoint();
                         waypointNumber = creep.getNextWayPointID();
