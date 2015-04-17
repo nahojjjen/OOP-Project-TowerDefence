3d888548e7ceaff5df91f11b9c17402136c8034d
nahojjjen <johansw95@gmail.com>
2015-04-17 10:38:51 +0200

    Removes the code simon made that I  pasted in
    Since I accidentally deleted simons work & copy pasted it back, it looks
    like I wrote the lines of code, so now im deleting them so he can add
    them back so they're correctly attributed to him. Sorry!

--- ProjectFiles/core/src/edu/chl/proximity/Models/CreepGenerator/StandardGenerator.java
+++ ProjectFiles/core/src/edu/chl/proximity/Models/CreepGenerator/StandardGenerator.java
@@ -26,58 +26,6 @@ public class StandardGenerator {
 
     }
 
-    public Wave generateWaveForWaveIndex(int waveIndex) {
 
-        //Defaults
-        double spawnInterval = 1;
-        double cooldown = 4;
-        ArrayList<Creep> creeps = new ArrayList<Creep>();
-
-        //First 5 waves are standard, after this spawning follows an algorithm
-        if (waveIndex <= 5) {
-
-            for (int i = 0; i < 5; i++) {
-                creeps.add(new Circle());
-            }
-
-            switch (waveIndex) {
-                case 2: {
-                    spawnInterval = 0.75;
-                }
-                break;
-                case 3: {
-                    for (int i = 0; i < 5; i++) {
-                        creeps.add(new Circle());
-                    }
-                }
-                break;
-                case 4: {
-                    for (int i = 0; i < 10; i++) {
-                        creeps.add(new Circle());
-                    }
-                    spawnInterval = 0.5;
-                }
-                break;
-                case 5: {
-                    for (int i = 0; i < 10; i++) {
-                        creeps.add(new Triangle());
-                    }
-                    spawnInterval = 1;
-                }
-                break;
-            }
-        } else {
-            for (int i = 0; i < waveIndex * 2; i++) {
-                creeps.add(new Circle());
-            }
-            for (int i = 0; i < waveIndex; i++) {
-                creeps.add(new Triangle());
-            }
-
-            spawnInterval = Math.max(3 / waveIndex, 0.1);
-
-        }
-        return new Wave(creeps, spawnInterval, cooldown);
-    }
 
 }
--- ProjectFiles/core/src/edu/chl/proximity/Models/Waves/Wave.java
+++ ProjectFiles/core/src/edu/chl/proximity/Models/Waves/Wave.java
@@ -10,27 +10,5 @@ package edu.chl.proximity.Models.Waves;
  * @date 2015-04-02
  */
 public class Wave {
-    private List<Creep> creeps;
-    private double spawnInterval;
-    private double cooldownTimeInterval;
-
-
-    public Wave(List<Creep> creeps, double spawnInterval, double cooldownTimeInterval) {
-        this.creeps = creeps;
-        this.spawnInterval = spawnInterval;
-        this.cooldownTimeInterval = cooldownTimeInterval;
-    }
-
-    //Getters
-    public List<Creep> getCreeps() {
-        return creeps;
-    }
-
-    public double getSpawnInterval() {
-        return spawnInterval;
-    }
-
-    public double getCooldownTimeInterval() {
-        return cooldownTimeInterval;
-    }
+   
 }
