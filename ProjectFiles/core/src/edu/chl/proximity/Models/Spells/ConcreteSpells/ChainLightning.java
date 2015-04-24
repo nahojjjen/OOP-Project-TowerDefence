68c9b3c5c8e5ec40ba4286633b50f6ce96984382
nahojjjen <johansw95@gmail.com>
2015-04-25 00:34:35 +0200

    Adds non-working chain lightning spell
    For this to work we need to add an AddStack or we get concurrent
    modificaiton (i think)

---
+++ ProjectFiles/core/src/edu/chl/proximity/Models/Spells/ConcreteSpells/ChainLightning.java
@@ -0,0 +1,50 @@
+package edu.chl.proximity.Models.Spells.ConcreteSpells;
+
+import com.badlogic.gdx.math.Vector2;
+import com.sun.net.httpserver.Filter;
+import edu.chl.proximity.Models.Creeps.Creep;
+import edu.chl.proximity.Models.GameData;
+import edu.chl.proximity.Models.Spells.PersistentObject;
+import edu.chl.proximity.Utilities.PointCalculations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Johan on 2015-04-24.
+ * A spell that creates an area of lightning, the lightning will "bounce" on nearby creeps
+ */
+public class ChainLightning extends PersistentObject {
+    private List<Creep> alreadyHitCreeps = new ArrayList<Creep>();
+    private double range = 100;
+
+    public ChainLightning(Vector2 position) {
+        super(position, 1); //600 frames = 10 seconds @ 60 fps
+        GameData.getInstance().getMap().getParticleManager().getLightningOriginSpellEffect().createEffect(position); //create original lightning effect
+
+    }
+
+    public ChainLightning(Vector2 position, List<Creep> alreadyHitCreeps) {
+        super(position, 1); //600 frames = 10 seconds @ 60 fps
+        range = 50;
+        this.alreadyHitCreeps = alreadyHitCreeps;
+
+    }
+
+    @Override
+    public void performEffect(int counter) {
+        List<Creep> creeps = GameData.getInstance().getMap().getCreeps();
+        for (Creep creep : creeps) {
+            if (PointCalculations.distanceBetweenNoSqrt(creep.getCenter(), getCenter()) < range * range) {
+                creep.devolve();
+                if (!alreadyHitCreeps.contains(creep)) {
+                    ChainLightning newSpark = new ChainLightning(creep.getCenter(), alreadyHitCreeps);
+                    GameData.getInstance().getMap().getParticleManager().getLightningCreepEffect().createEffect(creep.getCenter()); //create minor lightning effect
+                }
+
+
+            }
+        }
+
+    }
+}
