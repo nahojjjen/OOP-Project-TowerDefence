b5d9eb7eb15dba2e4eb049ef680c7ed842088cf8
nahojjjen <johansw95@gmail.com>
2015-04-25 00:35:23 +0200

    Adds wall of stone spell
    Has no particle effects yet

---
+++ ProjectFiles/core/src/edu/chl/proximity/Models/Spells/ConcreteSpells/WallOfStone.java
@@ -0,0 +1,30 @@
+package edu.chl.proximity.Models.Spells.ConcreteSpells;
+
+import com.badlogic.gdx.math.Vector2;
+import edu.chl.proximity.Models.Creeps.Creep;
+import edu.chl.proximity.Models.GameData;
+import edu.chl.proximity.Models.Spells.PersistentObject;
+import edu.chl.proximity.Utilities.PointCalculations;
+
+import java.util.List;
+
+/**
+ * @author Johan on 2015-04-25.
+ */
+public class WallOfStone extends PersistentObject {
+
+    public WallOfStone(Vector2 position) {
+        super(position, 600); //600 frames = 10 seconds @ 60 fps
+        GameData.getInstance().getMap().getParticleManager().getFrostField().createEffect(position);
+    }
+
+    @Override
+    public void performEffect(int counter) {
+        List<Creep> creeps = GameData.getInstance().getMap().getCreeps();
+        for (Creep creep : creeps) {
+            if (PointCalculations.distanceBetweenNoSqrt(creep.getCenter(), getCenter()) < 60 * 60) {
+                creep.slowDown(100, 1);
+            }
+        }
+    }
+}
