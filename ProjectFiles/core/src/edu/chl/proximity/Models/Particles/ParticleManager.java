d9dc21cc841cf036f9930b04ddeb8b3f6b5989cc
nahojjjen <johansw95@gmail.com>
2015-04-28 21:48:30 +0200

    Adds two beta spells

--- ProjectFiles/core/src/edu/chl/proximity/Models/Particles/ParticleManager.java
+++ ProjectFiles/core/src/edu/chl/proximity/Models/Particles/ParticleManager.java
@@ -33,6 +33,10 @@ public class ParticleManager {
         allEffects.add(new ProximityEffect("fireCreepEffect", 100));
         allEffects.add(new ProximityEffect("dirtSmokeEffect", 100));
         allEffects.add(new ProximityEffect("wallOfStoneSpell2", 1));
+        allEffects.add(new ProximityEffect("BloodPool", 1));
+        allEffects.add(new ProximityEffect("BloodPoolCreepEffect", 1));
+        allEffects.add(new ProximityEffect("BloodCarnage", 1));
+        allEffects.add(new ProximityEffect("BloodCarnageCreepEffect", 1));
     }
 
     /**
@@ -58,6 +62,10 @@ public class ParticleManager {
     public ProximityEffect getFireCreepEffect() {return allEffects.get(10);}
     public ProximityEffect getDirtSmokeEffect() {return allEffects.get(11);}
     public ProximityEffect getWallOfStone() {return allEffects.get(12);}
+    public ProximityEffect getBloodPoolEffect() {return allEffects.get(13);}
+    public ProximityEffect getBloodPoolCreepEffect() {return allEffects.get(14);}
+    public ProximityEffect getBloodCarnageEffect() {return allEffects.get(15);}
+    public ProximityEffect getBloodCarnageCreepEffect() {return allEffects.get(16);}
 
 }
 
