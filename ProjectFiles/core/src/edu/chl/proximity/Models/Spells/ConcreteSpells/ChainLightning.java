69f7076c34befe68d33b342004dfcfa4f41448e1
nahojjjen <johansw95@gmail.com>
2015-04-28 17:57:24 +0200

    Balances lightning spell
    Reworks logic for lightning spell, it no longer creates 470000 copies of
    itself if you use it optimally, it's less overpowered

--- ProjectFiles/core/src/edu/chl/proximity/Models/Spells/ConcreteSpells/ChainLightning.java
+++ ProjectFiles/core/src/edu/chl/proximity/Models/Spells/ConcreteSpells/ChainLightning.java
@@ -15,21 +15,26 @@ import java.util.List;
  * A spell that creates an area of lightning, the lightning will "bounce" on nearby creeps
  */
 public class ChainLightning extends PersistentObject {
-    private List<Creep> alreadyHitCreeps = new ArrayList<Creep>();
+    private List<Vector2> alreadyHitPositions = new ArrayList<Vector2>();
     private double range = 100;
-
+    private double bounceRange = 20;
+    private static int amount = 0;
     public ChainLightning(Vector2 position) {
-        super(position, 1); //600 frames = 10 seconds @ 60 fps
+        super(position, 2); //600 frames = 10 seconds @ 60 fps
         GameData.getInstance().getMap().getParticleManager().getLightningOriginSpellEffect().createEffect(position); //create original lightning effect
 
     }
 
-    public ChainLightning(Vector2 position, List<Creep> alreadyHitCreeps) {
-        super(position, 1); //600 frames = 10 seconds @ 60 fps
+    public ChainLightning(Vector2 position, List<Vector2> alreadyHitPositions) {
+        super(position, 2); //600 frames = 10 seconds @ 60 fps
         range = 100;
-        this.alreadyHitCreeps = alreadyHitCreeps;
+        this.alreadyHitPositions = alreadyHitPositions;
 
-        GameData.getInstance().getMap().getParticleManager().getLightningOriginSpellEffect().createEffect(position); //create original lightning effect
+        //debugg
+        amount++;
+        System.out.println(amount);
+
+        GameData.getInstance().getMap().getParticleManager().getLightningCreepEffect().createEffect(position); //create the spark effect
 
     }
 
@@ -38,16 +43,26 @@ public class ChainLightning extends PersistentObject {
         List<Creep> creeps = GameData.getInstance().getMap().getCreeps();
         for (Creep creep : creeps) {
             if (PointCalculations.distanceBetweenNoSqrt(creep.getCenter(), getCenter()) < range * range) {
-                creep.devolve();
-                if (!alreadyHitCreeps.contains(creep)) {
-                    //GameData.getInstance().getMap().getAddStack().add(new ChainLightning(creep.getCenter(), alreadyHitCreeps));
-                    ChainLightning newSpark = new ChainLightning(creep.getCenter(), alreadyHitCreeps);
-                    GameData.getInstance().getMap().getParticleManager().getLightningCreepEffect().createEffect(creep.getCenter()); //create minor lightning effect
+                creep.devolve();//devolve all creeps in range
+                if (!alreadyHit(creep.getCenter())) { //Create a new spark from the hit creep, if the area has not already bounced
+                    ChainLightning newSpark = new ChainLightning(creep.getCenter(), alreadyHitPositions);
+                    alreadyHitPositions.add(creep.getCenter());
                 }
-
-
             }
         }
+    }
 
+    /**
+     * cyckle through all the already hit positions, and see if the new position is within range of an already hit position
+     * @param position
+     * @return
+     */
+    private boolean alreadyHit(Vector2 position){
+        for (Vector2 hitPosition:alreadyHitPositions){
+            if (PointCalculations.distanceBetweenNoSqrt(position, hitPosition) < bounceRange*bounceRange){
+                return true;
+            }
+        }
+        return false;
     }
 }
