9cd808cc92209d52ca1d6498e49c463be3601187
nahojjjen <johansw95@gmail.com>
2015-05-04 19:33:55 +0200

    Initial test files for base and gameData

---
+++ ProjectFiles/core/src/test/edu/chl/proximity/Models/Map/Bases/BaseTest.java
@@ -0,0 +1,57 @@
+package test.edu.chl.proximity.Models.Map.Bases;
+
+import edu.chl.proximity.Models.Map.Bases.Base;
+import edu.chl.proximity.Models.Map.Particles.ParticleManager;
+import edu.chl.proximity.Models.Utils.GameData;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Johan
+ * @date 2015-05-03.
+ */
+public class BaseTest {
+
+    @Test
+    public void testGetLife() throws Exception {
+        GameData.createTestDataEnviroment();
+        Base test = new BaseConcreteClass(null,null,null);
+        assertTrue(test.getLife() == 100);
+    }
+
+    @Test
+    public void testSetLife() throws Exception {
+
+    }
+
+    @Test
+    public void testDamage() throws Exception {
+
+    }
+
+    @Test
+    public void testDamage1() throws Exception {
+
+    }
+
+    @Test
+    public void testHeal() throws Exception {
+
+    }
+
+    @Test
+    public void testShowDamageEffect() throws Exception {
+
+    }
+
+    @Test
+    public void testSetDamageEffect() throws Exception {
+
+    }
+
+    @Test
+    public void testSetCracksEffect() throws Exception {
+
+    }
+}
