070a1230eb6bad4f40479d8e0ba25ad0b21f5a4f
simongislen <simongislen@yahoo.in>
2015-05-04 15:39:36 +0200

    Added a sample test class

---
+++ ProjectFiles/core/src/test/edu/chl/proximity/Models/Maps/MapTest.java
@@ -0,0 +1,24 @@
+package test.edu.chl.proximity.Models.Maps;
+
+import junit.framework.TestCase;
+
+/**
+ *   @author Simon Gislen
+ *   @date 04/05/2015
+ * Test class for map
+ *
+ */
+public class MapTest extends TestCase {
+
+    public void testAddCreep() throws Exception {
+
+    }
+
+    public void testAddProjectile() throws Exception {
+
+    }
+
+    public void testAddTower() throws Exception {
+
+    }
+}
