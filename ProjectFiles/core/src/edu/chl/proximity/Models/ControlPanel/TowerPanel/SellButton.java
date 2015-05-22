be656d4cf81d92ddd30e47ecdf04ea87aaf519e8
floompa <linda@evaldsson.se>
2015-05-22 12:10:36 +0200

    Added modified Sell Button and restructured TowerPanel some, HashMap added for handling CheckButtons.

--- ProjectFiles/core/src/edu/chl/proximity/Models/ControlPanel/TowerPanel/SellButton.java
+++ ProjectFiles/core/src/edu/chl/proximity/Models/ControlPanel/TowerPanel/SellButton.java
@@ -16,42 +16,11 @@ import edu.chl.proximity.Utilities.ProximityVector;
  * A button for selling towers. Displays what resouces the choosen tower will be sold for
  */
 public class SellButton extends BoardObject{
-    private Map map;
 
-    private static Image image=new Image(Constants.FILE_PATH + "Buttons/SellButton.png");
-    private ProximityFont points;
-    private ProximityFont lines;
-    private ProximityFont polygons;
+    private static Image image=new Image(Constants.FILE_PATH + "Resources/trash.png");
 
     public SellButton(ProximityVector pos, Map map){
         super(pos,image,0);
-        this.map=map;
-        points=new ProximityFont(new ProximityVector(pos.x+85,pos.y+10), null);
-        lines=new ProximityFont(new ProximityVector(pos.x+85, pos.y+25),null);
-        polygons=new ProximityFont(new ProximityVector(pos.x+85,pos.y+40),null);
-        setInfo();
-    }
-
-    public void setInfo(){
-        if(map.getChosenTower()!=null){
-            Resources cost=map.getChosenTower().getCost();
-            Double p= new Double(cost.getPoints()/2);
-            Double l=new Double(cost.getLines()/2);
-            Double poly=new Double(cost.getPolygons()/2);
-            points.setText("Points " + p.intValue());
-            lines.setText("Lines "+ l.intValue());
-            polygons.setText("Polygons "+ poly.intValue());
-        }
-    }
-
-    public void render(ProximityBatch batch){
-        setInfo();
-        if(map.getChosenTower()!=null){
-            super.render(batch);
-            points.draw(batch);
-            lines.draw(batch);
-            polygons.draw(batch);
-        }
     }
 
 }
--- ProjectFiles/core/src/edu/chl/proximity/Models/ControlPanel/TowerPanel/TowerPanel.java
+++ ProjectFiles/core/src/edu/chl/proximity/Models/ControlPanel/TowerPanel/TowerPanel.java
@@ -6,7 +6,6 @@ import edu.chl.proximity.Models.Utils.ProximityBatch;
 import edu.chl.proximity.Utilities.ProximityVector;
 import edu.chl.proximity.Models.BoardObject;
 import edu.chl.proximity.Models.Map.Maps.Map;
-import edu.chl.proximity.Models.Map.Towers.ShootingTower;
 import edu.chl.proximity.Models.Map.Towers.TargetingMethods.*;
 import edu.chl.proximity.Models.Map.Towers.Tower;
 import edu.chl.proximity.Models.ResourceSystem.Resources;
@@ -15,9 +14,13 @@ import edu.chl.proximity.Models.Utils.Image;
 import edu.chl.proximity.Models.Utils.ProximityFont;
 import edu.chl.proximity.Utilities.Constants;
 
+import java.util.HashMap;
+
 /**
  * @author Hanna Romer
  * @date 2015-05-08
+ *
+ * A panel that handles changing settings and upgradings of towers
  *
  * 22/05 modified by Linda Evaldsson. Fixed bugs. Also redesigned Panel and moved it to ControlPanel (graphically).
  */
@@ -25,23 +28,20 @@ public class TowerPanel extends BoardObject{
     private Map map;
     private static Image background=new Image(Constants.FILE_PATH + "Backgrounds/tweed.png");
     private Image towerImage;
-    private static ProximityVector pos=new ProximityVector(Constants.GAME_WIDTH - 300,450); //651, 550
+    private static ProximityVector pos=new ProximityVector(Constants.GAME_WIDTH - 300,450);
     private static int width=300;
     private static int height=150;
     private TargetingFactory targetingFactory;
     private ProximityFont towerName;
     private ProximityVector towerPosition;
 
-    private CheckBox first;
-    private CheckBox closest;
-    private CheckBox last;
+
+    private HashMap<CheckBox, TargetingMethod> checkBoxMap = new HashMap<CheckBox, TargetingMethod>();
+
     private UpgradeButton upgrade;
     private SellButton sell;
 
     private ProximityFont upgradeText;
-    private ProximityFont points;
-    private ProximityFont lines;
-    private ProximityFont polygons;
     private ResourceDisplayerCollection upgradeCost;
 
     private boolean afford;
@@ -49,11 +49,11 @@ public class TowerPanel extends BoardObject{
     public TowerPanel(Map map){
         super(pos,background,0,width,height);
         this.map=map;
+        targetingFactory=new TargetingFactory();
         initiateCheckButtons();
         initiateUpgrade();
         initiateTower();
         initiateSell();
-        targetingFactory=new TargetingFactory();
 
 
         setInfo();
@@ -75,55 +75,38 @@ public class TowerPanel extends BoardObject{
 
     }
     private void initiateCheckButtons() {
-        first=new CheckBox(new ProximityVector(pos.x+30,pos.y + 80), map, "Target first");
-        closest=new CheckBox(new ProximityVector(pos.x+30,pos.y + 105), map, "Target closest");
-        last=new CheckBox(new ProximityVector(pos.x+30,pos.y + 130), map, "Target last");
+        checkBoxMap.put(new CheckBox(new ProximityVector(pos.x+30, pos.y + 80), map, "Target first"), targetingFactory.getTargetFirst());
+        checkBoxMap.put(new CheckBox(new ProximityVector(pos.x+30, pos.y + 105), map, "Target closest"), targetingFactory.getTargetClosest());
+        checkBoxMap.put(new CheckBox(new ProximityVector(pos.x+30, pos.y + 130), map, "Target last"), targetingFactory.getTargetLast());
+
     }
     private void initiateSell() {
         sell=new SellButton(new ProximityVector(pos.x+150,pos.y+115),map);
     }
     public BoardObject getButtonOnPosition(ProximityVector pos){
-        if(first.containsPoint(pos)){
-            pressedFirst();
-            return first;
-        }else if(closest.containsPoint(pos)){
-            pressedClosest();
-            return closest;
-        }else if(last.containsPoint(pos)){
-            pressedLast();
-            return last;
-        }else if(upgrade.containsPoint(pos)){
+
+        if(upgrade.containsPoint(pos)){
             pressedUpgrade();
         }else if(sell.containsPoint(pos)){
             pressedSell();
         }
+        for (HashMap.Entry<CheckBox, TargetingMethod> entry : checkBoxMap.entrySet())
+        {
+            if(entry.getKey().containsPoint(pos)) {
+                if(map.getChosenTower() instanceof TargetingTower) {
+                    ((TargetingTower) map.getChosenTower()).setTargetingMethod(entry.getValue());
+                    uncheckAll();
+                    entry.getKey().setAsChecked();
+                }
+            }
+        }
         return null;
     }
 
-    public void pressedFirst(){
-        if(map.getChosenTower() instanceof TargetingTower){
-            ((TargetingTower) map.getChosenTower()).setTargetingMethod(targetingFactory.getTargetFirst());
-            first.setAsChecked();
-            closest.setAsUnchecked();
-            last.setAsUnchecked();
-        }
-    }
-
-    public void pressedClosest(){
-        if(map.getChosenTower() instanceof TargetingTower){
-            ((TargetingTower) map.getChosenTower()).setTargetingMethod(targetingFactory.getTargetClosest());
-            first.setAsUnchecked();
-            closest.setAsChecked();
-            last.setAsUnchecked();
-        }
-    }
-
-    public void pressedLast(){
-        if(map.getChosenTower() instanceof TargetingTower){
-            ((TargetingTower) map.getChosenTower()).setTargetingMethod(targetingFactory.getTargetLast());
-            first.setAsUnchecked();
-            closest.setAsUnchecked();
-            last.setAsChecked();
+    private void uncheckAll() {
+        for (HashMap.Entry<CheckBox, TargetingMethod> entry : checkBoxMap.entrySet())
+        {
+            entry.getKey().setAsUnchecked();
         }
     }
 
@@ -176,12 +159,13 @@ public class TowerPanel extends BoardObject{
             }
             if(map.getChosenTower() instanceof TargetingTower){
                 TargetingTower chosenTargetingTower = (TargetingTower)chosenTower;
-                if(chosenTargetingTower.getTargetingMethod() instanceof TargetFirst){
-                    pressedFirst();
-                }else if(chosenTargetingTower.getTargetingMethod() instanceof TargetClosest){
-                    pressedClosest();
-                }else if(chosenTargetingTower.getTargetingMethod() instanceof TargetLast){
-                    pressedLast();
+                uncheckAll();
+
+                for (HashMap.Entry<CheckBox, TargetingMethod> entry : checkBoxMap.entrySet())
+                {
+                    if(entry.getValue().getClass() == chosenTargetingTower.getTargetingMethod().getClass()) {
+                        entry.getKey().setAsChecked();
+                    }
                 }
             }
         }
@@ -191,9 +175,11 @@ public class TowerPanel extends BoardObject{
         if(map.getChosenTower() != null) {
             setInfo();
             batch.renderRepeatedly(background, pos, width, height);
-            first.render(batch);
-            closest.render(batch);
-            last.render(batch);
+
+            for (HashMap.Entry<CheckBox, TargetingMethod> entry : checkBoxMap.entrySet())
+            {
+                entry.getKey().render(batch);
+            }
             towerName.draw(batch);
             batch.render(towerImage, towerPosition, 0);
             sell.render(batch);
