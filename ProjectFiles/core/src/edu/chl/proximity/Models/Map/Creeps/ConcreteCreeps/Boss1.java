71c7c8a4054d7d474bbc414bc5b6a7233ed11bcb
simongislen <simongislen@yahoo.in>
2015-05-25 12:22:53 +0200

    Added boss creep

---
+++ ProjectFiles/core/src/edu/chl/proximity/Models/Map/Creeps/ConcreteCreeps/Boss1.java
@@ -0,0 +1,11 @@
+package edu.chl.proximity.Models.Map.Creeps.ConcreteCreeps;
+
+/**
+ * @author simongislen.
+ * @date 25/05/15.
+ * <p/>
+ * <p/>
+ * A Boss1 class that is responsible for...
+ */
+public class Boss1 {
+}
