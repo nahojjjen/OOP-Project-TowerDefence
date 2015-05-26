4f9e63aa54b5ce3100d2dbd00b2bdcbc94b02d92
nahojjjen <johansw95@gmail.com>
2015-05-26 10:51:51 +0200

    Adds comments to proximity
    and clears up unused methods

--- ProjectFiles/core/src/edu/chl/proximity/Proximity.java
+++ ProjectFiles/core/src/edu/chl/proximity/Proximity.java
@@ -11,6 +11,9 @@ import edu.chl.proximity.Models.Map.Maps.Map;
 import edu.chl.proximity.Models.Player.Players.Player;
 
 /**
+ * This is the entry point of the appliaction. On application start a new Player is created, and the menu is loaded.
+ * The Game model does not load until the player enters a game. The player is loaded because the main menu items
+ * adjust depending on the player save data.
  *
  * 08/04 Modified by Johan Swanberg. Switch to ScreenType from GameState.
  * 08/04 modified by Linda Evaldsson. Changes in structure, removed SpriteBatch.
@@ -18,44 +21,18 @@ import edu.chl.proximity.Models.Player.Players.Player;
  */
 
 public class Proximity extends Game {
-	public enum State{GAME,MAIN_MENU,GAME_OVER}
-
-	public void changeScreen(State state, Map map, Player player, Viewport viewport){
-
-		switch(state){
-			case GAME:
-				this.setScreen(new GameScreen(this,map,player, viewport));
-				break;
-			case MAIN_MENU:
-				this.setScreen(new MenuScreen(this, player, viewport));
-				break;
-			case GAME_OVER:
-				this.setScreen(new GameOverScreen(this,map,player));
-		}
-	}
-	public State getCurrentScreen(){
-		if(this.getScreen() instanceof GameScreen){
-			return State.GAME;
-		}else{
-			return State.MAIN_MENU;
-		}
-	}
 
 	@Override
 	public void create () {
-		//A new player is created with no faction
 		Player player = new Player(null);
-
-		//The application switchs to the start screen menu screen
 		this.setScreen(new MenuScreen(this, player, null));
 	}
-
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1); //This is here so the previous frame does not flicker into the next frame
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
-		screen.render(Gdx.graphics.getDeltaTime());
+		screen.render(Gdx.graphics.getDeltaTime()); //render the applications current screen
 	}
 
 }
