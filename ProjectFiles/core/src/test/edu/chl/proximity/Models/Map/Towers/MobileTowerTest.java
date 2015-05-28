cfb3dacdf2e7cad5285585a275a28be469834434
simongislen <simongislen@yahoo.in>
2015-05-28 10:02:58 +0200

    Worked with tests

---
+++ ProjectFiles/core/src/test/edu/chl/proximity/Models/Map/Towers/MobileTowerTest.java
@@ -0,0 +1,55 @@
+package test.edu.chl.proximity.Models.Map.Towers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author simongislen.
+ * @date 28/05/15.
+ * <p/>
+ * <p/>
+ * A MobileTowerTest class that is responsible for...
+ */
+public class MobileTowerTest {
+
+    @Test
+    public void testRender() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+
+    }
+
+    @Test
+    public void testTarget() throws Exception {
+
+    }
+
+    @Test
+    public void testMove() throws Exception {
+
+    }
+
+    @Test
+    public void testCheckIfCollision() throws Exception {
+
+    }
+
+    @Test
+    public void testCreateProjectile() throws Exception {
+
+    }
+
+    @Test
+    public void testGetNewUpgrade() throws Exception {
+
+    }
+
+    @Test
+    public void testPreparePlacing() throws Exception {
+
+    }
+}
