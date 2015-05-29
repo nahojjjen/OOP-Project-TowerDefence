de9b84f23c4edb7815bb5cceb1a5514b6e2717c3
nahojjjen <johansw95@gmail.com>
2015-05-29 20:15:52 +0200

    Changes the appearance of the loss screen
    Adds a background image, and adjusts the button positions

--- ProjectFiles/core/src/edu/chl/proximity/Models/WonLostModels/GameOver.java
+++ ProjectFiles/core/src/edu/chl/proximity/Models/WonLostModels/GameOver.java
@@ -3,7 +3,6 @@ package edu.chl.proximity.Models.WonLostModels;
 import edu.chl.proximity.Models.Map.Maps.Map;
 import edu.chl.proximity.Models.Utils.Image;
 import edu.chl.proximity.Models.Utils.ProximityBatch;
-import edu.chl.proximity.Models.Utils.ProximityFont;
 import edu.chl.proximity.Utilities.Constants;
 import edu.chl.proximity.Utilities.ProximityVector;
 
@@ -18,20 +17,24 @@ public class GameOver {
     private Map map;
 
     private Button resume;
-    private ProximityVector rPos=new ProximityVector(430,400);
+    private ProximityVector startOverButtonPosition =new ProximityVector(400,350);
+    private ProximityVector mainMenuButtonPosition =new ProximityVector(630,350);
+    private ProximityVector backgroundPos =new ProximityVector(0,0);
+    private ProximityVector defeatImagePosition =new ProximityVector(500, 120);
 
     private Button mainMenu;
 
     private Image defeatImage = new Image(Constants.FILE_PATH + "Backgrounds/defeated.png");
 
-    private ProximityVector mmPos=new ProximityVector(630,400);
+
+    private Image background =new Image(Constants.FILE_PATH + "Backgrounds/lossScreenBackground.png");
 
 
     public GameOver(Map map){
         this.map=map;
 
-        mainMenu=new Button(mmPos, new Image(Constants.FILE_PATH + "Buttons/mainMenu.png"));
-        resume=new Button(rPos, new Image(Constants.FILE_PATH + "Buttons/startOver.png"));
+        mainMenu=new Button(mainMenuButtonPosition, new Image(Constants.FILE_PATH + "Buttons/mainMenu.png"));
+        resume=new Button(startOverButtonPosition, new Image(Constants.FILE_PATH + "Buttons/startOver.png"));
     }
 
     public String getButtonActionOnPosition(ProximityVector position) {
@@ -51,9 +54,10 @@ public class GameOver {
 
 
     public void render(ProximityBatch batch){
+        batch.render(background, backgroundPos, 0);
         mainMenu.render(batch);
         resume.render(batch);
-        batch.render(defeatImage, new ProximityVector(500, 120), 0);
+        batch.render(defeatImage, defeatImagePosition, 0);
 
     }
 }
